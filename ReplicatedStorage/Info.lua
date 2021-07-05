3c5a63ce823c77eb7af98eb0b5d97789b92a49b0
Senko <68160877+Senko3141@users.noreply.github.com>
2021-07-04 22:14:12 -0400

    Create Info.lua

---
+++ ReplicatedStorage/Info.lua
@@ -0,0 +1,21 @@
+return{
+	Tam = {
+		[1] = {
+			text = "Hello, welcome to whatever aldkfjas;dlfkjasd;flaskjdf.",
+			responses = {
+				"okay .-.",
+				"you deserve to be lynched..."
+			}
+		},
+		[2] = {
+			text = "2nd stuff loool",
+			responses = {
+				"okay ....",
+				"you deserve to die"
+			}
+		},
+		final = {
+			text = "thank you. now get out of my sight looool"
+		}
+	}
+}
